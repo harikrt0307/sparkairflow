326af659e8a2033185ecbbbaaf5886bc4cd25a8d
harikrt0307 <129357807+harikrt0307@users.noreply.github.com>
2024-05-08 13:02:11 +0530

    Create k8's_pod.py

---
+++ dags/k8's_pod.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+}
+
+with DAG('pyspark_job', default_args=default_args, schedule_interval=None) as dag:
+
+    pyspark_task = KubernetesPodOperator(
+        task_id='pyspark_task',
+        name='pyspark_job',
+        namespace='airflow',
+        image='kabileshe/newspark:3.5.1',
+        cmds=[
+            '/opt/spark/bin/spark-submit',
+            '--master=k8s://https://172.19.33.11:6443',
+            '--deploy-mode', 'cluster',
+            '--name', 'script',
+            '--conf', 'spark.kubernetes.authenticate.driver.serviceAccountName=newspark',
+            'local:///opt/spark/examples/jars/script1.py'
+        ],
+        is_delete_operator_pod=True,
+        get_logs=True
+    )

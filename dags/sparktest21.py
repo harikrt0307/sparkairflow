c1398a67831e797c3e54e4f7af329cb5d58dfeaa
harikrt0307 <129357807+harikrt0307@users.noreply.github.com>
2024-05-22 00:12:40 +0530

    Update sparktest21.py

--- dags/sparktest21.py
+++ dags/sparktest21.py
@@ -8,40 +8,29 @@ default_args = {
 }
 
 with DAG('run_pyspark_scripts', schedule_interval=None, default_args=default_args, catchup=False) as dag:
+
     script1 = KubernetesPodOperator(
         task_id='run_script1',
         name='spark-script1',
         namespace='spark-operator',
-        image='apache/spark:3.5.1',
+        image='apache/spark:3.5.1',  # Ensure the correct tag for Spark version
         env_vars={
             'SPARK_MASTER': 'k8s://https://172.19.33.11:6443',
         },
-        cmds=["/bin/bash", "-c", "python /tmp/script2.py"],
-        init_containers=[
-            {
-                'name': 'download-scripts',
-                'image': 'curlimages/curl',
-                'command': ['sh', '-c', 'curl https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript2.py -o /tmp/script2.py']
-            }
-        ]
+        cmds=["python"],
+        arguments=["https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript2.py"],
     )
 
     script2 = KubernetesPodOperator(
         task_id='run_script2',
         name='spark-script2',
         namespace='spark-operator',
-        image='apache/spark:3.5.1',
+        image='apache/spark:3.5.1',  # Ensure the correct tag for Spark version
         env_vars={
             'SPARK_MASTER': 'k8s://https://172.19.33.11:6443',
         },
-        cmds=["/bin/bash", "-c", "python /tmp/script1.py"],
-        init_containers=[
-            {
-                'name': 'download-scripts',
-                'image': 'curlimages/curl',
-                'command': ['sh', '-c', 'curl https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript.py -o /tmp/script1.py']
-            }
-        ]
+        cmds=["python"],
+        arguments=["https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript.py"],
     )
 
     script1 >> script2

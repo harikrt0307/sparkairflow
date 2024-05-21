2739beef55638b6d6341b01d2b3319eda9107f91
harikrt0307 <129357807+harikrt0307@users.noreply.github.com>
2024-05-22 00:09:52 +0530

    Update sparktest21.py

--- dags/sparktest21.py
+++ dags/sparktest21.py
@@ -8,7 +8,6 @@ default_args = {
 }
 
 with DAG('run_pyspark_scripts', schedule_interval=None, default_args=default_args, catchup=False) as dag:
-
     script1 = KubernetesPodOperator(
         task_id='run_script1',
         name='spark-script1',
@@ -17,7 +16,10 @@ with DAG('run_pyspark_scripts', schedule_interval=None, default_args=default_arg
         env_vars={
             'SPARK_MASTER': 'k8s://https://172.19.33.11:6443',
         },
-        cmds=["python", "https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript2.py"],
+        cmds=["/bin/bash", "-c", "python {{ ti.xcom_pull(task_ids='get_script1')[0] }}"],
+        get_files={
+            'https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript2.py': '/tmp/script2.py'
+        }
     )
 
     script2 = KubernetesPodOperator(
@@ -28,7 +30,10 @@ with DAG('run_pyspark_scripts', schedule_interval=None, default_args=default_arg
         env_vars={
             'SPARK_MASTER': 'k8s://https://172.19.33.11:6443',
         },
-        cmds=["python", "https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript.py"],
+        cmds=["/bin/bash", "-c", "python {{ ti.xcom_pull(task_ids='get_script2')[0] }}"],
+        get_files={
+            'https://raw.githubusercontent.com/harikrt0307/sparkairflow/main/dags/testscript.py': '/tmp/script1.py'
+        }
     )
 
     script1 >> script2

b1fe6c42b79390375a10c2a4781974008a93a0fb
harikrt0307 <129357807+harikrt0307@users.noreply.github.com>
2024-05-22 12:34:57 +0530

    Create gpscript3.py

---
+++ dags/gpscript3.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+
+# Create a SparkSession
+spark = SparkSession.builder.appName("InsertIntoGreenPlum").getOrCreate()
+
+# Configure GreenPlum connection details
+greenplum_host = "172.19.33.16"
+greenplum_port = "5432"
+greenplum_database = "gpadmin"
+greenplum_user = "gpadmin"
+greenplum_password = "gpadmin"
+
+# Read data from CSV into a DataFrame
+excel_file_path = "https://github.com/harikrt0307/sparkairflow/raw/main/dags/data1.csv"
+df = spark.read.format("csv") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load(excel_file_path)
+
+# Write the DataFrame to the GreenPlum table
+jdbc_url = f"jdbc:postgresql://{greenplum_host}:{greenplum_port}/{greenplum_database}"
+df.write.format("jdbc") \
+    .option("url", jdbc_url) \
+    .option("dbtable", "airflow_test") \
+    .option("user", greenplum_user) \
+    .option("password", greenplum_password) \
+    .mode("append") \
+    .save()
+
+# Stop the SparkSession
+spark.stop()

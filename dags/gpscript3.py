15313a9c99857fa0f8f314e853c54e3e000c356c
harikrt0307 <129357807+harikrt0307@users.noreply.github.com>
2024-05-22 12:44:36 +0530

    Update gpscript3.py

--- dags/gpscript3.py
+++ dags/gpscript3.py
@@ -10,19 +10,20 @@ greenplum_database = "gpadmin"
 greenplum_user = "gpadmin"
 greenplum_password = "gpadmin"
 
-# Read data from CSV into a DataFrame
-excel_file_path = "https://github.com/harikrt0307/sparkairflow/raw/main/dags/data1.csv"
-df = spark.read.csv(excel_file_path, header=True, inferSchema=True)
+# Read data from the GitHub URL into a DataFrame
+excel_file_url = "https://github.com/harikrt0307/sparkairflow/raw/main/dags/data1.csv"
+df = spark.read.option("inferSchema", "true").option("header", "true").csv(excel_file_url)
 
 # Write the DataFrame to the GreenPlum table
 jdbc_url = f"jdbc:postgresql://{greenplum_host}:{greenplum_port}/{greenplum_database}"
+
 df.write.format("jdbc") \
-    .option("url", jdbc_url) \
-    .option("dbtable", "airflow_test") \
-    .option("user", greenplum_user) \
-    .option("password", greenplum_password) \
-    .mode("append") \
-    .save()
+   .option("url", jdbc_url) \
+   .option("dbtable", "airflow_test") \
+   .option("user", greenplum_user) \
+   .option("password", greenplum_password) \
+   .mode("append") \
+   .save()
 
 # Stop the SparkSession
 spark.stop()

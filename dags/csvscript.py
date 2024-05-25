bb8f9332db1225ba0b771008e0cb86b948f06c37
harikrt0307 <129357807+harikrt0307@users.noreply.github.com>
2024-05-25 16:43:57 +0530

    Create csvscript.py

---
+++ dags/csvscript.py
@@ -0,0 +1,37 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+
+# Initialize SparkSession
+spark = SparkSession.builder \
+    .appName("CSV to DataFrame") \
+    .getOrCreate()
+
+# Define the schema for the CSV file
+schema = StructType([
+    StructField("emp_id", StringType(), True),
+    StructField("att_date", StringType(), True),
+    StructField("actual_intime", StringType(), True),
+    StructField("actual_outtime", StringType(), True),
+    StructField("regular_intime", StringType(), True),
+    StructField("regular_outtime", StringType(), True),
+    StructField("location", StringType(), True),
+    StructField("attendance", StringType(), True)
+])
+
+# Path to the CSV file
+csv_file_path = "/home/tkg-bootstrap/spark2204/sparkapplication/data.csv"
+
+# Read the CSV file into a DataFrame
+df = spark.read.format("csv") \
+    .option("header", "true") \
+    .schema(schema) \
+    .load(csv_file_path)
+
+# Show the DataFrame
+df.show()
+
+# Print the schema
+df.printSchema()
+
+# Stop the SparkSession
+spark.stop()
